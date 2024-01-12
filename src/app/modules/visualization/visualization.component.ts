9c4fdd7cf4bf631ab757817b07f6594467af9c36
Danny Pacheco <dannypachecodev@hotmail.com>
2024-01-12 15:35:04 -0500

    fix some issues at meet

--- src/app/modules/visualization/visualization.component.ts
+++ src/app/modules/visualization/visualization.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-visualization',
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./visualization.component.scss']
 })
 export class VisualizationComponent {
+  public showButton: boolean = false;
+  
+  constructor() { }
+  public detectScroll(event:any){
+    const element = document.getElementById('navbar'); // Get the native element
+    if(element){
+      const rect = element.getBoundingClientRect(); // Get the bounding rect of the element
+    
+      // Check if the element is in the viewport
+      const isVisible = (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+      if (isVisible) {
+        // Element is visible, perform your actions here
+        this.showButton = true;
+      } else {
+        // Element is not visible
+        this.showButton = false;
+      }
+    }
+  }
 }
