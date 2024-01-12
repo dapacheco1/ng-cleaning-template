70b0dec8e5c98372ce4104fe1919f491ee2b5ece
Danny Pacheco <dannypachecodev@hotmail.com>
2024-01-12 10:17:17 -0500

    feat(reviews): add reviews section

---
+++ src/app/modules/visualization/components/reviews/ProvisionalData.ts
@@ -0,0 +1,37 @@
+export const provisionalData = [
+    {
+        name: 'Dane S.',
+        rate: 5,
+        date: 'May 15, 2023',
+        avatar: '../../../../../assets/alone-boy-6129399_1280.jpg',
+        comment:'She cleaned most of my patio windows that’s the first time that’s ever been done by any of the girls I had clean my house she’s tops in my book I’ll give her a 10 if I could thanks girls',
+    },
+    {
+        name: 'Danielle F.',
+        rate: 5,
+        date: '3 weeks ago',
+        avatar: '../../../../../assets/girl-4376755_1280.jpg',
+        comment:'I had the most AMAZING experience with Jany and her team! They were so professional as they worked around my “work from home” setting and accommodated my dog as well! They did a deep cleaning and I feel as though I just moved into a new home! I am going to start booking monthly cleanings moving forward and I cannot give enough praises!!',
+    },
+    {
+        name: 'Laurie H.',
+        rate: 5,
+        date: 'Oct 21, 2023',
+        avatar: '../../../../../assets/girl-4376755_1280.jpg',
+        comment:'Very good first time cleaning.Was so pleased set up monthly cleaning.Easy to contact and very responsible.Pleasant crew. Professional',
+    },
+    {
+        name: 'Julia S.',
+        rate: 5,
+        date: 'Oct 4, 2023',
+        avatar: '../../../../../assets/girl-4376755_1280.jpg',
+        comment:'I have an emergency citation with my housekeeper and I need to find someone to do the job at my Airbnb quick and clean. She and her colleague did a great job! Thank you!',
+    },
+    {
+        name: 'Rhonda C.',
+        rate: 5,
+        date: 'Sep 15, 2023',
+        avatar: '../../../../../assets/girl-4376755_1280.jpg',
+        comment:'Jany was very nice and professional. She did a great job and was very aware of my cats... they like to rush the doors. She responded very quickly and when she came out to meet me... she had all her cleaning supplies and started to work right away!',
+    },
+];
--- src/app/modules/visualization/components/reviews/reviews.component.ts
+++ src/app/modules/visualization/components/reviews/reviews.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { provisionalData } from './ProvisionalData';
 
 @Component({
-  selector: 'app-reviews',
+  selector: 'cleaning-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent {
-
+    public data = provisionalData;
+    
 }
